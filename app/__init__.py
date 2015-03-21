ba4090744a287d5b8a36765dc001bf2861e3690e
njuwangchen <wangchenclark0512@gmail.com>
2015-03-21 12:35:32 +0800

    新建了按api模块划分的views文件，请每个人按文件分工，参考TRMSdemo项目的views文件编写！注意，先更新TRMSdemo项目。

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ __author__ = 'ClarkWong'
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cors import CORS
+from flask.ext.restful import Api
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 cor = CORS(app, allow_headers='Content-Type')
+
+api = Api(app)
 
 from app import models
