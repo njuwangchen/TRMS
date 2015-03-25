a1c8c838a1a40a2a55d0521ce9666f180f01b4be
Haoyu Yang <opop369a@icloud.com>
2015-03-25 23:34:27 +0800

    add del class

--- app/code_literature_views.py
+++ app/code_literature_views.py
@@ -13,6 +13,7 @@ code_literature_fields={
 }
 
 
+
 class code_literatureListApi(Resource):
 
     def __init__(self):
@@ -30,6 +31,7 @@ class code_literatureListApi(Resource):
         literature.codes.append(code)
         db.session.commit()
         return "add success" , 201
+
 
 class code_literatureQuery(Resource):
     def __init__(self):
@@ -49,6 +51,26 @@ class code_literatureQuery(Resource):
         else:
             abort(404, message='No code_literature at all')
 
+class code_literatureDel(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('code_id', type=int, required=True,location='json')
+        self.parser.add_argument('literature_id', type=int, required=True, location='json')
+        super(code_literatureDel, self).__init__()
+
+
+    def post(self):
+        args = self.parser.parse_args()
+        literature = Literature_meta.query.filter_by(id=args['literature_id']).first()
+        code = Code.query.filter_by(id=args['code_id']).first()
+        literature.codes.remove(code)
+        db.session.commit()
+        return "delete success" , 201
+
+
+
+
 
 api.add_resource(code_literatureQuery, '/api/v1/code_literatures/query', endpoint='code_literaturequery')
 api.add_resource(code_literatureListApi, '/api/v1/code_literatures', endpoint='code_literatureList')
+api.add_resource(code_literatureDel, '/api/v1/code_literatures/del', endpoint='code_literaturedel')
