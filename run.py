da49c23e93f32daca71c1917e3ef757f09618049
njuwangchen <wangchenclark0512@gmail.com>
2015-05-18 14:43:20 +0800

    修改host为0.0.0.0

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from flask.ext.cors import CORS
 cors = CORS(app, resources=r'/api/*', allow_headers='Content-Type')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0')
